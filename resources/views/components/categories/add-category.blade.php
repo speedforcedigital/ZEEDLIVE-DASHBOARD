da2af4fd9da3b3184014ea6daa753ef3ac9b0f42
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2023-06-29 13:43:51 +0300

    Updated

--- resources/views/components/categories/add-category.blade.php
+++ resources/views/components/categories/add-category.blade.php
@@ -119,6 +119,9 @@
               <option>Model 4</option>
               <option>Model 5</option>
             </select>
+
+            <button class="btn bg-white dark:bg-slate-800 border-slate-200 dark:border-slate-700 hover:border-slate-300 dark:hover:border-slate-600 text-rose-500">Delete</button>
+
             <button class="btn dark:bg-slate-800 border-slate-200 dark:border-slate-700 c18nt czq29 ciajw">
               <svg class="c7hxs cz1vo c29x4 cq5uz" viewBox="0 0 16 16">
                 <path d="M5 7h2v6H5V7zm4 0h2v6H9V7zm3-6v2h4v2h-1v10c0 .6-.4 1-1 1H2c-.6 0-1-.4-1-1V5H0V3h4V1c0-.6.4-1 1-1h6c.6 0 1 .4 1 1zM6 2v1h4V2H6zm7 3H3v9h10V5z"></path>
