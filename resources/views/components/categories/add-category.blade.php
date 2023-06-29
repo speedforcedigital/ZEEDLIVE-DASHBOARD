ea0747e0e6e27583c71613f6d1953f5c2383f96c
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2023-06-29 16:41:54 +0300

    Updated

--- resources/views/components/categories/add-category.blade.php
+++ resources/views/components/categories/add-category.blade.php
@@ -86,7 +86,7 @@
             <div>
               <label class="block text-sm font-medium mb-1" for="brand">Brand <span class="text-rose-500">*</span></label>
               <div class="flex">
-                <input id="brand" class="form-input w-full" type="text" wire:model="selectedBrand" wire:change="selectBrand($event.target.value)" required />
+                <input id="brand" class="form-input w-full" type="text" required />
                 <button class="btn bg-indigo-500 hover:bg-indigo-600 text-white ml-2" wire:click="addBrand">
                   <svg class="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
                     <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
