b372da579c615b8d055932f81462e899896040bc
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2023-06-29 14:37:59 +0300

    Updated

--- resources/views/components/categories/add-category.blade.php
+++ resources/views/components/categories/add-category.blade.php
@@ -121,7 +121,7 @@
             <select size="5" class="custom-select mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 wire:model="selectedModel"
                 wire:change="selectModel($event.target.value)"
-                @if (!$selectedBrand) disabled @endif>
+                @if (!isset($selectedBrand) || empty($selectedBrand)) disabled @endif>
                 <option value="">Select a model</option>
                 @if ($selectedBrand)
                     @foreach ($selectedBrand->models as $model)
