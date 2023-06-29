b3e217ac7fe81631948dfee4b8bf974103ddfbca
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2023-06-29 12:47:33 +0300

    Updated

--- resources/views/components/categories/add-category.blade.php
+++ resources/views/components/categories/add-category.blade.php
@@ -1,167 +1,88 @@
-<div class="row">
-    <div class="mb-2 col-xl-6">
-        <div class="card">
-            <div class="card-header d-flex">
-                <span class="flex-grow-1">Add Country</span>
-            </div>
-            <div class="card-body">
-                <h4 class="card-title">Country</h4>
-                <label>Add a Country</label>
-                <div class="input-group">
-                    <input type="text" placeholder="Write country name" class="form-control" id="__BVID__45">
-                    <div class="input-group-append">
-                        <button type="button" disabled="disabled" class="btn btn-primary disabled">
-                            <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="plus square" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-plus-square b-icon bi">
-                                <g>
-                                    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
-                                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
-                                </g>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-                <hr class="invisible mb-2">
-                <fieldset class="form-group" id="__BVID__46">
-                    <legend tabindex="-1" class="bv-no-focus-ring col-form-label pt-0" id="__BVID__46__BV_label_">Select Country</legend>
+<div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+    <!-- Page header -->
+
+    <div class="border-t border-slate-200">
+
+        <!-- Components -->
+        <div class="space-y-8 mt-8">
+
+            <!-- Input Types -->
+            <div>
+                <div class="grid gap-5 md:grid-cols-3"> <!-- Changed to 3 columns -->
+
                     <div>
-                        <div class="input-group">
-                            <select class="custom-select" id="__BVID__47">
-                                <option disabled="disabled" value="">-- Please select a country --</option>
-                                <option value="22">Saudi Arabia</option>
-                                <option value="23">United Arab Emirates</option>
-                                <option value="26">Oman</option>
-                                <option value="27">Kuwait</option>
-                                <option value="31">Bahrain</option>
-                                <option value="33">aaa</option>
-                                <option value="34">demo</option>
-                                <option value="35">Malta</option>
+                        <!-- Start -->
+                        <div>
+                            <label class="block text-sm font-medium mb-1" for="category">Category <span class="text-rose-500">*</span></label>
+                            <div class="flex">
+                                <input id="category" class="form-input w-full" type="text" wire:model="category" required />
+                                <button class="btn bg-indigo-500 hover:bg-indigo-600 text-white ml-2" wire:click="addCategory">
+                                    <svg class="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
+                                        <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+                                    </svg>
+                                </button>
+                            </div>
+                            @error('category')<div class="text-xs mt-1 text-rose-500">{{ $message }}</div>@enderror
+                            <select class="custom-select mt-2" disabled>
+                                <!-- Populate with categories from backend -->
+                                <option>Category 1</option>
+                                <option>Category 2</option>
+                                <option>Category 3</option>
                             </select>
-                            <div class="input-group-append">
-                                <button type="button" class="btn btn-danger">
-                                    <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="trash" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-trash b-icon bi">
-                                        <g>
-                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
-                                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
-                                        </g>
+                        </div>
+                        <!-- End -->
+                    </div>
+
+                    <div>
+                        <!-- Start -->
+                        <div>
+                            <label class="block text-sm font-medium mb-1" for="brand">Brand <span class="text-rose-500">*</span></label>
+                            <div class="flex">
+                                <input id="brand" class="form-input w-full" type="text" wire:model="brand" required />
+                                <button class="btn bg-indigo-500 hover:bg-indigo-600 text-white ml-2" wire:click="addBrand">
+                                    <svg class="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
+                                        <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
                                     </svg>
                                 </button>
                             </div>
-                        </div>
-                    </div>
-                </fieldset>
-            </div>
-        </div>
-    </div>
-    <div class="mb-2 col-xl-6">
-        <div class="card">
-            <div class="card-header d-flex">
-                <span class="flex-grow-1">Add city for the selected country</span>
-            </div>
-            <div class="card-body">
-                <h4 class="card-title">City</h4>
-                <label>Add a City</label>
-                <div class="input-group">
-                    <input type="text" placeholder="Write city name" class="form-control" id="__BVID__48">
-                    <div class="input-group-append">
-                        <button type="button" disabled="disabled" class="btn btn-primary disabled">
-                            <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="plus square" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-plus-square b-icon bi">
-                                <g>
-                                    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
-                                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
-                                </g>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-                <hr class="invisible mb-2">
-                <fieldset class="form-group" id="__BVID__49">
-                    <legend tabindex="-1" class="bv-no-focus-ring col-form-label pt-0" id="__BVID__49__BV_label_">Select city</legend>
-                    <div>
-                        <div class="input-group">
-                            <select class="custom-select" id="__BVID__50">
-                                <option disabled="disabled" value="">-- Please select a city --</option>
-                                <option value="67">Jeddah</option>
-                                <option value="68">Riyadh</option>
-                                <option value="79">Dammam</option>
-                                <option value="80">Abha</option>
-                                <option value="81">Qasim</option>
-                                <option value="82">Hafof</option>
-                                <option value="83">Hail</option>
-                                <option value="84">Jazan</option>
-                                <option value="85">Madinah</option>
-                                <option value="86">Makkah</option>
-                                <option value="87">Taif</option>
-                                <option value="88">Tabouk</option>
+                            @error('brand')<div class="text-xs mt-1 text-rose-500">{{ $message }}</div>@enderror
+                            <select size="7" class="custom-select mt-2" disabled>
+                                <!-- Populate with brands from backend -->
+                                <option>Brand 1</option>
+                                <option>Brand 2</option>
+                                <option>Brand 3</option>
                             </select>
-                            <div class="input-group-append">
-                                <button type="button" class="btn btn-danger">
-                                    <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="trash" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-trash b-icon bi">
-                                        <g>
-                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
-                                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
-                                        </g>
+                        </div>
+                        <!-- End -->
+                    </div>
+
+                    <div>
+                        <!-- Start -->
+                        <div>
+                            <label class="block text-sm font-medium mb-1" for="model">Model <span class="text-rose-500">*</span></label>
+                            <div class="flex">
+                                <input id="model" class="form-input w-full" type="text" wire:model="model" required />
+                                <button class="btn bg-indigo-500 hover:bg-indigo-600 text-white ml-2" wire:click="addModel">
+                                    <svg class="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
+                                        <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
                                     </svg>
                                 </button>
                             </div>
+                            @error('model')<div class="text-xs mt-1 text-rose-500">{{ $message }}</div>@enderror
+                            <select size="7" class="custom-select mt-2" disabled>
+                                <!-- Populate with models from backend -->
+                                <option>Model 1</option>
+                                <option>Model 2</option>
+                                <option>Model 3</option>
+                            </select>
                         </div>
+                        <!-- End -->
                     </div>
-                </fieldset>
+
+                </div>
             </div>
+
         </div>
-    </div>
-    <div class="mb-2 col-xl-12">
-        <div class="card">
-            <div class="card-header d-flex">
-                <span class="flex-grow-1">Add branches for the selected city</span>
-            </div>
-            <div class="card-body">
-                <h4 class="card-title">Branches</h4>
-                <fieldset class="form-group" id="__BVID__51">
-                    <div>
-                        <label>Branch Name</label>
-                        <div class="input-group">
-                            <input placeholder="Write branch name" class="form-control">
-                            <div class="input-group-append">
-                                <button type="button" disabled="disabled" class="btn btn-primary disabled">
-                                    <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="plus square" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-plus-square b-icon bi">
-                                        <g>
-                                            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
-                                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
-                                        </g>
-                                    </svg>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </fieldset>
-                <fieldset class="form-group" id="__BVID__52">
-                    <div>
-                        <select size="7" class="custom-select" id="__BVID__53">
-                            <option value="47">Red Sea Mall</option>
-                            <option value="48">Aziz Mall</option>
-                            <option value="50">Andulus Mall</option>
-                            <option value="51">Mall of Arabia</option>
-                            <option value="52">Flamingo Mall</option>
-                            <option value="53">Haifa Mall</option>
-                            <option value="54">Mahmal Plaza</option>
-                            <option value="55">Salam Mall</option>
-                            <option value="56">Yasmeen Mall</option>
-                        </select>
-                    </div>
-                </fieldset>
-                <fieldset class="form-group" id="__BVID__54">
-                    <div>
-                        <button type="button" class="btn btn-danger disabled" disabled="disabled">Remove Selected Branch
-                            <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="trash" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-trash b-icon bi">
-                                <g>
-                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
-                                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
-                                </g>
-                            </svg>
-                        </button>
-                    </div>
-                </fieldset>
-            </div>
-        </div>
+
     </div>
 </div>
