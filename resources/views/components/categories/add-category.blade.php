588b266709e2a0c29fff143d2dc50fa45322cfaf
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2023-06-29 16:23:48 +0300

    Updated

--- resources/views/components/categories/add-category.blade.php
+++ resources/views/components/categories/add-category.blade.php
@@ -1,4 +1,4 @@
-@props(['categories', 'selectedCategory'])
+@props(['categories', 'selectedCategory', 'selectedBrandId'])
 
 <style>
 .custom-select {
@@ -86,7 +86,7 @@
             <div>
               <label class="block text-sm font-medium mb-1" for="brand">Brand <span class="text-rose-500">*</span></label>
               <div class="flex">
-                <input id="brand" class="form-input w-full" type="text" wire:model="selectedBrand" required />
+                <input id="brand" class="form-input w-full" type="text" wire:model="selectedBrand" wire:change="selectBrand($event.target.value)" required />
                 <button class="btn bg-indigo-500 hover:bg-indigo-600 text-white ml-2" wire:click="addBrand">
                   <svg class="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
                     <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
@@ -95,7 +95,7 @@
               </div>
               @error('selectedBrand')<div class="text-xs mt-1 text-rose-500">{{ $message }}</div>@enderror
               <select size="5" class="custom-select mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    wire:model="selectedBrand"
+                    wire:model="selectedBrandId"
                     wire:change="selectBrand($event.target.value)"
                     @if (!isset($selectedCategory) || empty($selectedCategory)) disabled @endif>
                     <option value="">Select a brand</option>
