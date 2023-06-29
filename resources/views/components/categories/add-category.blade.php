1f18da17197794f470d3c0866f5c848bcdb57fe1
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2023-06-29 12:11:00 +0300

    Updated

--- resources/views/components/categories/add-category.blade.php
+++ resources/views/components/categories/add-category.blade.php
@@ -1,63 +1,70 @@
 <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-        <!-- Page header -->
-        
+    <!-- Page header -->
 
-        <div class="border-t border-slate-200">
+    <div class="border-t border-slate-200">
 
-            <!-- Components -->
-            <div class="space-y-8 mt-8">
+        <!-- Components -->
+        <div class="space-y-8 mt-8">
 
-                <!-- Input Types -->
-                <div>
-                    
-                    <div class="grid gap-5 md:grid-cols-2">
-                        
+            <!-- Input Types -->
+            <div>
+                <div class="grid gap-5 md:grid-cols-3"> <!-- Changed to 3 columns -->
+
+                    <div>
+                        <!-- Start -->
                         <div>
-                            <!-- Start -->
-                            <div>
-                                <label class="block text-sm font-medium mb-1" for="mandatory">Name <span class="text-rose-500">*</span></label>
-                                <input id="mandatory" class="form-input w-full" type="text" wire:model="name" required />
-                                @error('name')<div class="text-xs mt-1 text-rose-500">{{ $message }}</div>@enderror
+                            <label class="block text-sm font-medium mb-1" for="category">Category <span class="text-rose-500">*</span></label>
+                            <div class="flex">
+                                <input id="category" class="form-input w-full" type="text" wire:model="category" required />
+                                <button class="btn bg-indigo-500 hover:bg-indigo-600 text-white ml-2" wire:click="addCategory">
+                                    <svg class="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
+                                        <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+                                    </svg>
+                                </button>
                             </div>
-                            <!-- End -->
+                            @error('category')<div class="text-xs mt-1 text-rose-500">{{ $message }}</div>@enderror
                         </div>
+                        <!-- End -->
+                    </div>
 
+                    <div>
+                        <!-- Start -->
                         <div>
-                            <!-- Start -->
-                            <div>
-                                <label class="block text-sm font-medium mb-1" for="mandatory">Image <span class="text-rose-500">*</span></label>
-                                <input id="mandatory" class="form-input w-full" type="file" wire:model="image" required />
-                                @error('image')<div class="text-xs mt-1 text-rose-500">{{ $message }}</div>@enderror
+                            <label class="block text-sm font-medium mb-1" for="brand">Brand <span class="text-rose-500">*</span></label>
+                            <div class="flex">
+                                <input id="brand" class="form-input w-full" type="text" wire:model="brand" required />
+                                <button class="btn bg-indigo-500 hover:bg-indigo-600 text-white ml-2" wire:click="addBrand">
+                                    <svg class="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
+                                        <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+                                    </svg>
+                                </button>
                             </div>
-                            <!-- End -->
+                            @error('brand')<div class="text-xs mt-1 text-rose-500">{{ $message }}</div>@enderror
                         </div>
+                        <!-- End -->
+                    </div>
 
-                   </div>
-                <input type="hidden" wire:model="category_id">
-                <div class="grid gap-5 pt-0 float-right md:grid-cols-2">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div>
-                   <button wire:click="addCategory()" wire:loading.class="opacity-50 cursor-wait" wire:target="addCategory" class="btn bg-indigo-500 hover:bg-indigo-600 text-white">
-                    <svg class="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
-                        <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
-                    </svg>
-                    <span class="hidden xs:block ml-2">
-                    <span wire:loading wire:target="addCategory" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                        <?= $this->updateMode ? 'Update Category' : 'Add Category' ?>
-                    </span>
-                </button>
-               </div>
+                    <div>
+                        <!-- Start -->
+                        <div>
+                            <label class="block text-sm font-medium mb-1" for="model">Model <span class="text-rose-500">*</span></label>
+                            <div class="flex">
+                                <input id="model" class="form-input w-full" type="text" wire:model="model" required />
+                                <button class="btn bg-indigo-500 hover:bg-indigo-600 text-white ml-2" wire:click="addModel">
+                                    <svg class="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
+                                        <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+                                    </svg>
+                                </button>
+                            </div>
+                            @error('model')<div class="text-xs mt-1 text-rose-500">{{ $message }}</div>@enderror
+                        </div>
+                        <!-- End -->
+                    </div>
 
                 </div>
-                   
-                </div>
-
             </div>
 
         </div>
 
     </div>
+</div>
