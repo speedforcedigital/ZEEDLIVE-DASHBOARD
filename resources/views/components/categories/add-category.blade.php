f0f9fc0fce864520bf9f4eff9876ee9a97405b02
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2023-06-29 13:49:40 +0300

    Updated

--- resources/views/components/categories/add-category.blade.php
+++ resources/views/components/categories/add-category.blade.php
@@ -122,8 +122,9 @@
 
             <button class="btn bg-white dark:bg-slate-800 border-slate-200 dark:border-slate-700 hover:border-slate-300 dark:hover:border-slate-600 text-rose-500">
                 <span class="mr-1">Delete</span>
-                <svg class="w-4 h-4 fill-current opacity-75" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd" d="M14 6a2 2 0 0 1-2-2V3H8v1a2 2 0 0 1-4 0V3H2v2a2 2 0 0 1-2 2v1h16V9a2 2 0 0 1-2-2V6zM6 4v1a1 1 0 0 0 2 0V4H6zm4 0v1h2V4h-2zM4 7v7a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2V7H4zm7 0v7h2V7h-2z"/>
+                <svg class="w-4 h-4 fill-current opacity-75" viewBox="0 0 16 16">
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
+                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
                 </svg>
             </button>
 
