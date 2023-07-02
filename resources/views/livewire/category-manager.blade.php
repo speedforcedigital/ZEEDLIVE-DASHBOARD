fdcedf05a61fe2dae6014e3fa1d42cc46ccb541e
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2023-07-03 02:18:36 +0300

    Updated

--- resources/views/livewire/category-manager.blade.php
+++ resources/views/livewire/category-manager.blade.php
@@ -154,7 +154,15 @@ foreach ($permissionsArray as $item) {
                         <div class="bg-white dark:bg-slate-800 rounded shadow-lg overflow-auto max-w-lg w-full max-h-full" @click.outside="deleteModalOpen = false" @keydown.escape.window="deleteModalOpen = false" style="max-width: 640px;">
                             <!-- Modal header -->
                             <div class="px-5 py-3 border-b border-slate-200 dark:border-slate-700">
-                                <div class="font-semibold text-slate-800 dark:text-slate-100">Remove Category</div>
+                                <div class="flex justify-between items-center">
+                                    <div class="font-semibold text-slate-800 dark:text-slate-100">Delete Category</div>
+                                    <button class="text-slate-400 dark:text-slate-500 hover:text-slate-500 dark:hover:text-slate-400" @click="deleteModalOpen = false">
+                                        <div class="sr-only">Close</div>
+                                        <svg class="w-4 h-4 fill-current">
+                                            <path d="M7.95 6.536L12.192 2.293a1 1 0 111.414 1.414L9.364 7.95l4.243 4.243a1 1 0 11-1.414 1.415L7.95 9.364l-4.243 4.243a1 1 0 01-1.414-1.415L6.536 7.95 2.293 3.707a1 1 0 011.414-1.414L7.95 6.536z" />
+                                        </svg>
+                                    </button>
+                                </div>
                             </div>
                             <!-- Modal content -->
                             <div class="px-5 py-4">
