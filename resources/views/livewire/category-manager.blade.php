f908a467136b69e0c868a36c67c5ccf41df615ca
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2023-07-03 02:47:49 +0300

    Updated

--- resources/views/livewire/category-manager.blade.php
+++ resources/views/livewire/category-manager.blade.php
@@ -139,7 +139,7 @@ foreach ($permissionsArray as $item) {
                   </button>
 
                       <!-- Delete Category Modal -->
-                      <div x-data="{ deleteModalOpen: @entangle('deleteModalOpen'), collectionsCount: 0 }">
+                      <div x-data="{ deleteModalOpen: @entangle('deleteModalOpen'), collectionsCount: @entangle('collectionsCount') }">
                         <button class="btn bg-indigo-500 hover:bg-indigo-600 text-white ml-2" @click="deleteModalOpen = true" style="height: 38px;">
                             <svg class="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
                                 <path d="M5 7h2v6H5V7zm4 0h2v6H9V7zm3-6v2h4v2h-1v10c0 .6-.4 1-1 1H2c-.6 0-1-.4-1-1V5H0V3h4V1c0-.6.4-1 1-1h6c.6 0 1 .4 1 1zM6 2v1h4V2H6zm7 3H3v9h10V5z"></path>
