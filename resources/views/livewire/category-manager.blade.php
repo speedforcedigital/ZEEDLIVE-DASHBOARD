90a8a72da2bc8fb056258d7a1ec15f141c32a974
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2023-07-02 21:44:29 +0300

    Updated

--- resources/views/livewire/category-manager.blade.php
+++ resources/views/livewire/category-manager.blade.php
@@ -108,12 +108,12 @@ foreach ($permissionsArray as $item) {
                         x-cloak
                     >
                         <!-- Modal content -->
-                        <div class="bg-white dark:bg-slate-800 rounded shadow-lg overflow-auto max-w-lg w-full max-h-full max-w-screen-md" @click.outside="modalOpen = false" @keydown.escape.window="modalOpen = false">
+                        <div class="bg-white dark:bg-slate-800 rounded shadow-lg overflow-auto max-w-lg w-full max-h-full" @click.outside="editModalOpen = false" @keydown.escape.window="editModalOpen = false" style="max-width: 640px;">
                             <!-- Modal header -->
                             <div class="px-5 py-3 border-b border-slate-200 dark:border-slate-700">
                                 <div class="flex justify-between items-center">
                                     <div class="font-semibold text-slate-800 dark:text-slate-100">Edit Category Name</div>
-                                    <button class="text-slate-400 dark:text-slate-500 hover:text-slate-500 dark:hover:text-slate-400" @click="modalOpen = false">
+                                    <button class="text-slate-400 dark:text-slate-500 hover:text-slate-500 dark:hover:text-slate-400" @click="editModalOpen = false">
                                         <div class="sr-only">Close</div>
                                         <svg class="w-4 h-4 fill-current">
                                             <path d="M7.95 6.536l4.242-4.243a1 1 0 111.415 1.414L9.364 7.95l4.243 4.242a1 1 0 11-1.415 1.415L7.95 9.364l-4.243 4.243a1 1 0 01-1.414-1.415L6.536 7.95 2.293 3.707a1 1 0 011.414-1.414L7.95 6.536z" />
