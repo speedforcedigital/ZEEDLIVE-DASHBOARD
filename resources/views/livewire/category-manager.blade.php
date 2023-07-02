923afbdc936ce2908aa0ec2e85fc87219d2e8c26
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2023-07-02 22:13:48 +0300

    Updated

--- resources/views/livewire/category-manager.blade.php
+++ resources/views/livewire/category-manager.blade.php
@@ -83,52 +83,53 @@ foreach ($permissionsArray as $item) {
 
                   <button class="btn bg-indigo-500 hover:bg-indigo-600 text-white ml-2" x-data="{ modalOpen: false }" @click="modalOpen = true" x-show="!selectedCategory">
                     <svg class="w-4 h-4 fill-current opacity-50 shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12">
-                      <g fill="currentColor" stroke="currentColor" class="nc-icon-wrapper">
-                        <line x1="8" y1="2" x2="10" y2="4" fill="none" stroke-linecap="round" stroke-linejoin="round" data-color="color-2"></line>
-                        <path d="M4,10l7.08-7.05A1.435,1.435,0,1,0,9.05.92L2,8,.5,11.5Z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
-                      </g>
+                    <g fill="currentColor" stroke="currentColor" class="nc-icon-wrapper">
+                    <line x1="8" y1="2" x2="10" y2="4" fill="none" stroke-linecap="round" stroke-linejoin="round" data-color="color-2"></line>
+                    <path d="M4,10l7.08-7.05A1.435,1.435,0,1,0,9.05.92L2,8,.5,11.5Z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </g>
                     </svg>
-                  </button>
+                </button>
+
 
                   <!-- Modal backdrop -->
                   <div class="fixed inset-0 bg-slate-900 bg-opacity-30 z-50 transition-opacity" x-show="modalOpen" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition ease-out duration-100" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" aria-hidden="true" x-cloak></div>
 
-                  <!-- Modal dialog -->
                   <div id="feedback-modal" class="fixed inset-0 z-50 overflow-hidden flex items-center my-4 justify-center px-4 sm:px-6" role="dialog" aria-modal="true" x-show="modalOpen" x-transition:enter="transition ease-in-out duration-200" x-transition:enter-start="opacity-0 translate-y-4" x-transition:enter-end="opacity-100 translate-y-0" x-transition:leave="transition ease-in-out duration-200" x-transition:leave-start="opacity-100 translate-y-0" x-transition:leave-end="opacity-0 translate-y-4" aria-hidden="true" x-cloak>
                     <!-- Modal content -->
                     <div class="bg-white dark:bg-slate-800 rounded shadow-lg overflow-auto max-w-lg w-full max-h-full" @click.outside="modalOpen = false" @keydown.escape.window="modalOpen = false" style="max-width: 640px;">
-                      <!-- Modal header -->
-                      <div class="px-5 py-3 border-b border-slate-200 dark:border-slate-700">
+                        <!-- Modal header -->
+                        <div class="px-5 py-3 border-b border-slate-200 dark:border-slate-700">
                         <div class="flex justify-between items-center">
-                          <div class="font-semibold text-slate-800 dark:text-slate-100">Edit Category Name</div>
-                          <button class="text-slate-400 dark:text-slate-500 hover:text-slate-500 dark:hover:text-slate-400" @click="modalOpen = false">
+                            <div class="font-semibold text-slate-800 dark:text-slate-100">Edit Category Name</div>
+                            <button class="text-slate-400 dark:text-slate-500 hover:text-slate-500 dark:hover:text-slate-400" @click="modalOpen = false">
                             <div class="sr-only">Close</div>
                             <svg class="w-4 h-4 fill-current">
-                              <path d="M7.95 6.536l4.242-4.243a1 1 0 111.415 1.414L9.364 7.95l4.243 4.242a1 1 0 11-1.415 1.415L7.95 9.364l-4.243 4.243a1 1 0 01-1.414-1.415L6.536 7.95 2.293 3.707a1 1 0 011.414-1.414L7.95 6.536z" />
+                                <path d="M7.95 6.536l4.242-4.243a1 1 0 111.415 1.414L9.364 7.95l4.243 4.242a1 1 0 11-1.415 1.415L7.95 9.364l-4.243 4.243a1 1 0 01-1.414-1.415L6.536 7.95 2.293 3.707a1 1 0 011.414-1.414L7.95 6.536z" />
                             </svg>
-                          </button>
+                            </button>
                         </div>
-                      </div>
-                      <!-- Modal content -->
-                      <div class="px-5 py-4">
+                        </div>
+                        <!-- Modal content -->
+                        <div class="px-5 py-4">
                         <div class="text-sm">
-                          <div class="font-medium text-slate-800 dark:text-slate-100 mb-3">Category Name</div>
-                          <div class="text-slate-800 dark:text-slate-100 mb-3">Editing {{ $selectedCategory ? $selectedCategory->name : '' }}</div>
+                            <div class="font-medium text-slate-800 dark:text-slate-100 mb-3">Category Name</div>
+                            <div class="text-slate-800 dark:text-slate-100 mb-3">Editing {{ $selectedCategory ? $selectedCategory->name : '' }}</div>
                         </div>
                         <div>
-                          <input id="category-name" class="form-input w-full px-2 py-1" type="text" wire:model="newCategoryName" required>
-                          @error('newCategoryName')<div class="text-xs mt-1 text-rose-500">{{ $message }}</div>@enderror
+                            <input id="category-name" class="form-input w-full px-2 py-1" type="text" wire:model="newCategoryName" required>
+                            @error('newCategoryName')<div class="text-xs mt-1 text-rose-500">{{ $message }}</div>@enderror
                         </div>
-                      </div>
-                      <!-- Modal footer -->
-                      <div class="px-5 py-4 border-t border-slate-200 dark:border-slate-700">
+                        </div>
+                        <!-- Modal footer -->
+                        <div class="px-5 py-4 border-t border-slate-200 dark:border-slate-700">
                         <div class="flex flex-wrap justify-end space-x-2">
-                            <button class="btn bg-indigo-500 hover:bg-indigo-600 text-white ml-2" x-data="{ modalOpen: false }" @click="modalOpen = false"> Cancel </button>
-                          <button class="btn-sm bg-indigo-500 hover:bg-indigo-600 text-white" wire:click="updateCategory">Update</button>
+                            <button class="btn bg-indigo-500 hover:bg-indigo-600 text-white ml-2" x-data="{ modalOpen: false }" @click="modalOpen = false">Cancel</button>
+                            <button class="btn-sm bg-indigo-500 hover:bg-indigo-600 text-white" wire:click="updateCategory">Update</button>
                         </div>
-                      </div>
+                        </div>
                     </div>
-                  </div>
+                    </div>
+
 
                   <button class="btn bg-indigo-500 hover:bg-indigo-600 text-white ml-2" wire:click="resetSelection">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="4 4 16 16">
