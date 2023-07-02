8c9b84dd39c6e77cfeef720a53e915eebda0f7a7
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2023-07-02 21:30:56 +0300

    Updated

--- resources/views/livewire/category-manager.blade.php
+++ resources/views/livewire/category-manager.blade.php
@@ -64,6 +64,52 @@ foreach ($permissionsArray as $item) {
         </div>
     </div>
 
+    <!-- Edit Category Modal -->
+<div x-data="{ isOpen: @entangle('modalOpen') }">
+    <button class="btn c5e6b cdsge c7qs0" @click.prevent="isOpen = true" aria-controls="feedback-modal">Edit Category</button>
+    <!-- Modal backdrop -->
+    <div class="bg-slate-900 cw1h6 cn21v cryht cl8sr cmayk" x-show="isOpen" x-transition:enter="c64rw cuqwi cl6tz" x-transition:enter-start="opacity-0" x-transition:enter-end="c0inn" x-transition:leave="c64rw cuqwi ctadz" x-transition:leave-start="c0inn" x-transition:leave-end="opacity-0" aria-hidden="true" x-cloak></div>
+    <!-- Modal dialog -->
+    <div id="feedback-modal" class="flex items-center justify-center cejl6 c2ksj cryht cl8sr cmayk cdmac cn2cr" role="dialog" aria-modal="true" x-show="isOpen" x-transition:enter="c64rw cw5sv cl6tz" x-transition:enter-start="opacity-0 c1x7j" x-transition:enter-end="c0inn csmxb" x-transition:leave="c64rw cw5sv cl6tz" x-transition:leave-start="c0inn csmxb" x-transition:leave-end="opacity-0 c1x7j" x-cloak>
+        <div class="bg-white dark:bg-slate-800 rounded cm7c4 c5gd2 c21iq cym6n c96ud" @click.outside="isOpen = false" @keydown.escape.window="isOpen = false">
+            <!-- Modal header -->
+            <div class="border-slate-200 dark:border-slate-700 cqyqv c7q80 cks8x">
+                <div class="flex items-center cc2cs">
+                    <div class="text-slate-800 dark:text-slate-100 ca8f5">Edit Category</div>
+                    <button class="cccp8 c3d0i camw1 c4cuk" @click="isOpen = false">
+                        <div class="czsjw">Close</div>
+                        <svg class="c7hxs c29x4 cq5uz">
+                            <path d="M7.95 6.536l4.242-4.243a1 1 0 111.415 1.414L9.364 7.95l4.243 4.242a1 1 0 11-1.415 1.415L7.95 9.364l-4.243 4.243a1 1 0 01-1.414-1.415L6.536 7.95 2.293 3.707a1 1 0 011.414-1.414L7.95 6.536z"></path>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <!-- Modal content -->
+            <div class="c7q80 cpnas">
+                <div class="text-sm">
+                    <div class="text-slate-800 dark:text-slate-100 cz4nn cm3dd">Edit the category name</div>
+                </div>
+                <div class="cfohq">
+                    <div>
+                        <label class="block text-sm cz4nn ca5ph" for="category-name">Category Name <span class="ciajw">*</span></label>
+                        <input id="category-name" class="c04hc c96ud czv4r cwkie" type="text" wire:model="name" required>
+                        @error('name')<div class="text-xs mt-1 text-rose-500">{{ $message }}</div>@enderror
+                    </div>
+                </div>
+            </div>
+            <!-- Modal footer -->
+            <div class="border-slate-200 dark:border-slate-700 cannk c7q80 cpnas">
+                <div class="flex flex-wrap justify-end cb50h">
+                    <button class="border-slate-200 dark:border-slate-700 c18nt czq29 cywsn c24n3 cijix" @click="isOpen = false">Cancel</button>
+                    <button class="c5e6b cdsge c7qs0 cijix" wire:click="updateCategory">Save</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Edit Category Modal -->
+
+
   <div class="border-t border-slate-200">
     <!-- Components -->
     <div class="space-y-8 mt-8">
@@ -217,47 +263,3 @@ foreach ($permissionsArray as $item) {
 </div>
 </div>
 
-<!-- Edit Category Modal -->
-<div x-data="{ isOpen: @entangle('modalOpen') }">
-    <button class="btn c5e6b cdsge c7qs0" @click.prevent="isOpen = true" aria-controls="feedback-modal">Edit Category</button>
-    <!-- Modal backdrop -->
-    <div class="bg-slate-900 cw1h6 cn21v cryht cl8sr cmayk" x-show="isOpen" x-transition:enter="c64rw cuqwi cl6tz" x-transition:enter-start="opacity-0" x-transition:enter-end="c0inn" x-transition:leave="c64rw cuqwi ctadz" x-transition:leave-start="c0inn" x-transition:leave-end="opacity-0" aria-hidden="true" x-cloak></div>
-    <!-- Modal dialog -->
-    <div id="feedback-modal" class="flex items-center justify-center cejl6 c2ksj cryht cl8sr cmayk cdmac cn2cr" role="dialog" aria-modal="true" x-show="isOpen" x-transition:enter="c64rw cw5sv cl6tz" x-transition:enter-start="opacity-0 c1x7j" x-transition:enter-end="c0inn csmxb" x-transition:leave="c64rw cw5sv cl6tz" x-transition:leave-start="c0inn csmxb" x-transition:leave-end="opacity-0 c1x7j" x-cloak>
-        <div class="bg-white dark:bg-slate-800 rounded cm7c4 c5gd2 c21iq cym6n c96ud" @click.outside="isOpen = false" @keydown.escape.window="isOpen = false">
-            <!-- Modal header -->
-            <div class="border-slate-200 dark:border-slate-700 cqyqv c7q80 cks8x">
-                <div class="flex items-center cc2cs">
-                    <div class="text-slate-800 dark:text-slate-100 ca8f5">Edit Category</div>
-                    <button class="cccp8 c3d0i camw1 c4cuk" @click="isOpen = false">
-                        <div class="czsjw">Close</div>
-                        <svg class="c7hxs c29x4 cq5uz">
-                            <path d="M7.95 6.536l4.242-4.243a1 1 0 111.415 1.414L9.364 7.95l4.243 4.242a1 1 0 11-1.415 1.415L7.95 9.364l-4.243 4.243a1 1 0 01-1.414-1.415L6.536 7.95 2.293 3.707a1 1 0 011.414-1.414L7.95 6.536z"></path>
-                        </svg>
-                    </button>
-                </div>
-            </div>
-            <!-- Modal content -->
-            <div class="c7q80 cpnas">
-                <div class="text-sm">
-                    <div class="text-slate-800 dark:text-slate-100 cz4nn cm3dd">Edit the category name</div>
-                </div>
-                <div class="cfohq">
-                    <div>
-                        <label class="block text-sm cz4nn ca5ph" for="category-name">Category Name <span class="ciajw">*</span></label>
-                        <input id="category-name" class="c04hc c96ud czv4r cwkie" type="text" wire:model="name" required>
-                        @error('name')<div class="text-xs mt-1 text-rose-500">{{ $message }}</div>@enderror
-                    </div>
-                </div>
-            </div>
-            <!-- Modal footer -->
-            <div class="border-slate-200 dark:border-slate-700 cannk c7q80 cpnas">
-                <div class="flex flex-wrap justify-end cb50h">
-                    <button class="border-slate-200 dark:border-slate-700 c18nt czq29 cywsn c24n3 cijix" @click="isOpen = false">Cancel</button>
-                    <button class="c5e6b cdsge c7qs0 cijix" wire:click="updateCategory">Save</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- End Edit Category Modal -->
