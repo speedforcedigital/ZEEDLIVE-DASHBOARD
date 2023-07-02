bb7501d799df58b10689d8d722613f8be317b81b
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2023-07-03 02:00:29 +0300

    Updated

--- resources/views/livewire/category-manager.blade.php
+++ resources/views/livewire/category-manager.blade.php
@@ -99,12 +99,12 @@ foreach ($permissionsArray as $item) {
                     <!-- Modal dialog -->
                     <div id="feedback-modal" class="fixed inset-0 z-50 overflow-hidden flex items-center my-4 justify-center px-4 sm:px-6" role="dialog" aria-modal="true" x-show="editModalOpen" x-transition:enter="transition ease-in-out duration-200" x-transition:enter-start="opacity-0 translate-y-4" x-transition:enter-end="opacity-100 translate-y-0" x-transition:leave="transition ease-in-out duration-200" x-transition:leave-start="opacity-100 translate-y-0" x-transition:leave-end="opacity-0 translate-y-4" aria-hidden="true" x-cloak>
                         <!-- Modal content -->
-                        <div class="bg-white dark:bg-slate-800 rounded shadow-lg overflow-auto max-w-lg w-full max-h-full" @click.outside="modalOpen = false" @keydown.escape.window="modalOpen = false" style="max-width: 640px;">
+                        <div class="bg-white dark:bg-slate-800 rounded shadow-lg overflow-auto max-w-lg w-full max-h-full" @click.outside="editModalOpen = false" @keydown.escape.window="editModalOpen = false" style="max-width: 640px;">
                             <!-- Modal header -->
                             <div class="px-5 py-3 border-b border-slate-200 dark:border-slate-700">
                                 <div class="flex justify-between items-center">
                                     <div class="font-semibold text-slate-800 dark:text-slate-100">Edit Category Name</div>
-                                    <button class="text-slate-400 dark:text-slate-500 hover:text-slate-500 dark:hover:text-slate-400" @click="modalOpen = false">
+                                    <button class="text-slate-400 dark:text-slate-500 hover:text-slate-500 dark:hover:text-slate-400" @click="editModalOpen = false">
                                         <div class="sr-only">Close</div>
                                         <svg class="w-4 h-4 fill-current">
                                             <path d="M7.95 6.536L12.192 2.293a1 1 0 111.414 1.414L9.364 7.95l4.243 4.243a1 1 0 11-1.414 1.415L7.95 9.364l-4.243 4.243a1 1 0 01-1.414-1.415L6.536 7.95 2.293 3.707a1 1 0 011.414-1.414L7.95 6.536z" />
@@ -151,7 +151,7 @@ foreach ($permissionsArray as $item) {
                   <!-- Modal dialog -->
                   <div id="delete-category-modal" class="fixed inset-0 z-50 overflow-hidden flex items-center my-4 justify-center px-4 sm:px-6" role="dialog" aria-modal="true" x-show="deleteModalOpen" x-transition:enter="transition ease-in-out duration-200" x-transition:enter-start="opacity-0 translate-y-4" x-transition:enter-end="opacity-100 translate-y-0" x-transition:leave="transition ease-in-out duration-200" x-transition:leave-start="opacity-100 translate-y-0" x-transition:leave-end="opacity-0 translate-y-4" aria-hidden="true" x-cloak>
                         <!-- Modal content -->
-                        <div class="bg-white dark:bg-slate-800 rounded shadow-lg overflow-auto max-w-lg w-full max-h-full" @click.outside="modalOpen = false" @keydown.escape.window="modalOpen = false" style="max-width: 640px;">
+                        <div class="bg-white dark:bg-slate-800 rounded shadow-lg overflow-auto max-w-lg w-full max-h-full" @click.outside="deleteModalOpen = false" @keydown.escape.window="deleteModalOpen = false" style="max-width: 640px;">
                             <!-- Modal header -->
                             <div class="px-5 py-3 border-b border-slate-200 dark:border-slate-700">
                                 <div class="font-semibold text-slate-800 dark:text-slate-100">Remove Category</div>
@@ -170,7 +170,7 @@ foreach ($permissionsArray as $item) {
                             <!-- Modal footer -->
                             <div class="px-5 py-4 border-t border-slate-200 dark:border-slate-700">
                                 <div class="flex justify-end">
-                                    <button class="btn-sm bg-indigo-500 hover:bg-indigo-600 text-white" @click="modalOpen = false">OK</button>
+                                    <button class="btn-sm bg-indigo-500 hover:bg-indigo-600 text-white" @click="deleteModalOpen = false">OK</button>
                                 </div>
                             </div>
                         </div>
