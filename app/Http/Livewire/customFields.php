e0df22b3cbf4bba77f9babcc45f11c8bf25a8042
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2023-07-03 15:42:20 +0300

    Updated

--- app/Http/Livewire/customFields.php
+++ app/Http/Livewire/customFields.php
@@ -1,101 +1,98 @@
 <?php
+
 namespace App\Http\Livewire;
-use App\Helpers\MakeCurlRequest;
-use App\Helpers\makeCurlPostRequest;
-use App\Helpers\baseUrl;
+
+use App\Models\CustomField;
+use App\Models\Category;
 use Livewire\Component;
-use Illuminate\Pagination\LengthAwarePaginator;
-class customFields extends Component
+use Livewire\WithPagination;
+
+class CustomFields extends Component
 {
-    public $custom_field_title, $category_id, $type ,$custom_field_id;
+    use WithPagination;
+
+    public $custom_field_title, $category_id, $type, $custom_field_id;
     public $addCustomField = false;
     public $updateMode = false;
     public $categoryList = false;
     public $fields = [];
+
+    protected $rules = [
+        'custom_field_title' => 'required',
+        'category_id' => 'required',
+        'type' => 'required',
+    ];
+
     public function render()
     {
-    $url = baseUrl().'get/customFields/all';
-    $data = makeCurlRequest($url, 'GET');
-    $customfields = $data['data']['customFields'];
-    $customfields_count = count($customfields); 
-    //pagination
-    $page = request()->query('page', 1);
-    $perPage = 10;
-    $customfields = new LengthAwarePaginator(
-        array_slice($customfields, ($page - 1) * $perPage, $perPage),
-        count($customfields),
-        $perPage,
-        $page,
-        [
-            'path' => request()->url(),
-            'query' => request()->query()
-        ]
-    );
-    
+        $customfields = CustomField::paginate(10);
+        $customfields_count = CustomField::count();
+
         return view('livewire.customfields', compact('customfields', 'customfields_count'));
-     
     }
 
     public function add()
     {
-        $url = baseUrl().'list/category';
-        $categoryList = makeCurlRequest($url, 'GET');
-        $this->categoryList = $categoryList;
+        $this->categoryList = Category::pluck('name', 'id')->toArray();
         $this->addCustomField = true;
     }
 
     public function updated($field)
     {
-        $validatedDate = $this->validateOnly($field,[
-            'custom_field_title' => 'required',
-            'category_id' => 'required',
-            'type' => 'required',
-        ]);
+        $this->validateOnly($field);
     }
 
     public function addCustomField()
     {
-        $validatedDate = $this->validate([
-            'custom_field_title' => 'required',
-            'category_id' => 'required',
-            'type' => 'required',
-        ]);
-        $postData['custom_field_title'] = $this->custom_field_title;
-        $postData['category_id'] = $this->category_id;
-        $postData['type'] = $this->type;
-        $postData['values'] = json_encode($this->fields);
-        $postData['custom_field_id'] = $this->custom_field_id;
-        $postData = json_encode($postData);
-        $url = baseUrl()."add/customFields";
-        $data = makeCurlPostRequest($url, 'POST',$postData);
-        if($data['success']=true)
-        {
-            $this->dispatchBrowserEvent('alert', 
-                    ['type' => 'success',  'message' => ''.$data['message'].'']);
+        $this->validate();
+
+        $customFieldData = [
+            'custom_field_title' => $this->custom_field_title,
+            'category_id' => $this->category_id,
+            'type' => $this->type,
+            'values' => json_encode($this->fields),
+        ];
+
+        if ($this->custom_field_id) {
+            $customField = CustomField::find($this->custom_field_id);
+            if ($customField) {
+                $customField->update($customFieldData);
+            }
+        } else {
+            $customField = CustomField::create($customFieldData);
         }
+
+        if ($customField) {
+            $this->dispatchBrowserEvent('alert', [
+                'type' => 'success',
+                'message' => 'Custom field saved successfully!',
+            ]);
+        }
+
         $this->addCustomField = false;
         $this->updateMode = false;
         $this->resetInputFields();
-
-
     }
+
     private function resetInputFields()
     {
         $this->custom_field_title = '';
         $this->category_id = '';
         $this->type = '';
         $this->custom_field_id = '';
+        $this->fields = [];
     }
 
     public function delete($id)
-    { 
-    $url = baseUrl()."delte/dynamicField/custom/".$id;
-    $data = makeCurlRequest($url, 'DELETE');
-    if($data['success']=true)
     {
-        $this->dispatchBrowserEvent('alert', 
-                ['type' => 'success',  'message' => ''.$data['message'].'']);
-    }
+        $customField = CustomField::find($id);
+        if ($customField) {
+            $customField->delete();
+            $this->dispatchBrowserEvent('alert', [
+                'type' => 'success',
+                'message' => 'Custom field deleted successfully!',
+            ]);
+        }
     }
 
     public function addField()
@@ -111,22 +108,18 @@ class customFields extends Component
 
     public function edit($id)
     {
-        $url = baseUrl().'list/category';
-        $categoryList = makeCurlRequest($url, 'GET');
-        $this->categoryList = $categoryList;
-        $url = baseUrl()."detail/customFields/".$id;
-        $data = makeCurlRequest($url, 'GET');
-        $singleCustomField = $data['data'];
-        $this->custom_field_title =   $singleCustomField[0]['custom_field_title'];
-        $this->category_id = $singleCustomField[0]['category_id'];
-        $this->type = $singleCustomField[0]['type'];
-        $this->custom_field_id = $singleCustomField[0]['custom_field_id'];
-        $array = json_decode($singleCustomField[0]['values']);
-        if(!empty($array))
-        {
-            $this->fields = $array;
+        $this->categoryList = Category::pluck('name', 'id')->toArray();
+        $customField = CustomField::find($id);
+        if ($customField) {
+            $this->custom_field_title = $customField->custom_field_title;
+            $this->category_id = $customField->category_id;
+            $this->type = $customField->type;
+            $this->custom_field_id = $customField->id;
+            $array = json_decode($customField->values);
+            if (!empty($array)) {
+                $this->fields = $array;
+            }
+            $this->updateMode = true;
         }
-        $this->updateMode = true;
     }
-    
 }
--- resources/views/livewire/customfields.blade.php
+++ resources/views/livewire/customfields.blade.php
@@ -1,71 +1,65 @@
 <?php
 $array = Session::get('permissions');
-//add
+// Add capability
 $add_capability_exists = false;
 $permissionsArray = json_decode($array, true);
 foreach ($permissionsArray as $item) {
-  if (isset($item['Custom Field']) && in_array('add', $item['Custom Field'])) {
-    $add_capability_exists = true;
-    break;
-  }
+    if (isset($item['Custom Field']) && in_array('add', $item['Custom Field'])) {
+        $add_capability_exists = true;
+        break;
+    }
 } 
-//list
+// List capability
 $list_capability_exists = false;
 $permissionsArray = json_decode($array, true);
 foreach ($permissionsArray as $item) {
-  if (isset($item['Custom Field']) && in_array('list', $item['Custom Field'])) {
-    $list_capability_exists = true;
-    break;
-  }
+    if (isset($item['Custom Field']) && in_array('list', $item['Custom Field'])) {
+        $list_capability_exists = true;
+        break;
+    }
 }
 ?>
 <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
 
-        <!-- Page header -->
-        <div class="sm:flex sm:justify-between sm:items-center mb-8">
+    <!-- Page header -->
+    <div class="sm:flex sm:justify-between sm:items-center mb-8">
 
-            <!-- Left: Title -->
-            <div class="mb-4 sm:mb-0">
-                <h1 class="text-2xl md:text-3xl text-slate-800 font-bold">
-            @if($addCustomField)
-            Add Custom Field ✨
-            @else
-            Custom Fields ✨
-            @endif   
-            </h1>
-            </div>
-
-            <!-- Right: Actions -->
-            <div class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
-
-                <!-- Delete button -->
-                <x-actions.delete-button />
-
-                <!-- Dropdown -->
-                <!-- <x-date-select /> -->
-
-                <!-- Filter button -->
-                <!-- <x-dropdown-filter align="right" /> -->
-                <!-- Add customer button -->
-                @if(!$this->addCustomField && $add_capability_exists && !$this->updateMode)
-                <button wire:click="add()" class="btn border-slate-200 hover:border-slate-300 bg-indigo-500 text-white">Add Custom Field</button>
+        <!-- Left: Title -->
+        <div class="mb-4 sm:mb-0">
+            <h1 class="text-2xl md:text-3xl text-slate-800 font-bold">
+                @if($addCustomField)
+                Add Custom Field ✨
+                @else
+                Custom Fields ✨
                 @endif
-            </div>
-
+            </h1>
         </div>
 
-        <!-- Table -->
-        @if($this->addCustomField)
-       <x-customfields.add-customfields />
-        @elseif($this->updateMode)
-        <x-customfields.add-customfields />
-        @else
-        @if($list_capability_exists)
-       <x-customfields.customfields-table :customfields="$customfields" :count="$customfields_count" />
-        @endif
-        <!-- Pagination -->
-        <div class="mt-8">
-                {{$customfields->links()}}
-            </div>
+        <!-- Right: Actions -->
+        <div class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
+
+            <!-- Delete button -->
+            <x-actions.delete-button />
+
+            <!-- Add custom field button -->
+            @if(!$addCustomField && $add_capability_exists && !$updateMode)
+            <button wire:click="add()" class="btn border-slate-200 hover:border-slate-300 bg-indigo-500 text-white">Add Custom Field</button>
+            @endif
         </div>
-        @endif
+
+    </div>
+
+    <!-- Table -->
+    @if($addCustomField || $updateMode)
+    <x-customfields.add-customfields />
+    @else
+    @if($list_capability_exists)
+    <x-customfields.customfields-table :customfields="$customfields" :count="$customfields_count" />
+    @endif
+    <!-- Pagination -->
+    <div class="mt-8">
+        {{$customfields->links()}}
+    </div>
+    @endif
+
+</div>
