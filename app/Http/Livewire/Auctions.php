e4b9afc13e9eff6c00e07d61b39f7251c24e821e
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2023-08-01 12:55:47 +0300

    Fixing auctions page

--- app/Http/Livewire/Auctions.php
+++ app/Http/Livewire/Auctions.php
@@ -1,87 +1,97 @@
 <?php
+
 namespace App\Http\Livewire;
-use App\Helpers\MakeCurlRequest;
-use App\Helpers\makeCurlPostRequest;
-use App\Helpers\baseUrl;
+
 use Livewire\Component;
 use Illuminate\Pagination\LengthAwarePaginator;
+use App\Models\Auction; // Replace "Auction" with your actual Eloquent model name
+
 class Auctions extends Component
 {
     public $filterAuction = false;
     public $filterType = '';
+
     public function render()
     {
-    $url = baseUrl().'list/all/auction';
-    $data = makeCurlRequest($url, 'GET');
-    // echo "<pre>";print_r($data);die();
-    if($this->filterAuction || $this->filterType)
-    {
-        $auctions = $this->filterAuction;
-        $total_auctions = count($auctions);
-    }
-    else
-    {
-    $auctions = $data['data'];
-    $total_auctions = count($auctions);
-    }
-    //pagination
-    $page = request()->query('page', 1);
-    $perPage = 10;
-    $auctions = new LengthAwarePaginator(
-        array_slice($auctions, ($page - 1) * $perPage, $perPage),
-        count($auctions),
-        $perPage,
-        $page,
-        [
-            'path' => request()->url(),
-            'query' => request()->query()
-        ]
-    );
+        // Replace this with your actual Eloquent query to fetch all auctions from the database
+        $query = Auction::query();
+        
+        if ($this->filterAuction || $this->filterType) {
+            // If filters are applied, adjust the query accordingly
+            if ($this->filterType === 'verified') {
+                $query->where('status', 'verified');
+            } elseif ($this->filterType === 'rejected') {
+                $query->where('status', 'rejected');
+            } elseif ($this->filterType === 'pending') {
+                $query->where('status', 'pending');
+            }
+            
+            $auctions = $query->get();
+            $total_auctions = count($auctions);
+        } else {
+            // If no filters are applied, fetch all auctions
+            $auctions = $query->get();
+            $total_auctions = count($auctions);
+        }
+
+        // Pagination
+        $page = request()->query('page', 1);
+        $perPage = 10;
+        $auctions = new LengthAwarePaginator(
+            $auctions->forPage($page, $perPage),
+            $total_auctions,
+            $perPage,
+            $page,
+            [
+                'path' => request()->url(),
+                'query' => request()->query(),
+            ]
+        );
+
         return view('livewire.auctions', compact('auctions', 'total_auctions'));
     }
 
     public function filterAuction($filterAuction)
     {  
-    if($filterAuction=='verified')
+        if ($filterAuction === 'verified') {
+            $this->filterAuctionByStatus('verified');
+        } elseif ($filterAuction === 'rejected') {
+            $this->filterAuctionByStatus('rejected');
+        } elseif ($filterAuction === 'pending') {
+            $this->filterAuctionByStatus('pending');
+        } else {
+            $this->filterAuctionByStatus('all');
+        }
+    }
+
+    private function filterAuctionByStatus($status)
     {
-      $filter = 'list/approved/auction';
-      $filterType='verified';
+        // Replace this with your actual Eloquent query to filter auctions by status
+        $this->filterAuction = Auction::where('status', $status)->get();
+        $this->filterType = $status;
     }
-    elseif($filterAuction=='rejected')
+
+    public function approved($id, $collection_id)
     {
-        $filter = 'list/rejected/auction';
-        $filterType='rejected';
+        // Replace this with your actual Eloquent code to update the status of the auction
+        $collectionStatus = Collection::find($collection_id); // Replace "Collection" with your actual Eloquent model name for collections
+        $collectionStatus->status = 'approved';
+        $collectionStatus->save();
+        
+        $auction = Auction::find($id); // Replace "Auction" with your actual Eloquent model name
+        $auction->status = 'approved';
+        $auction->save();
+
+        $this->dispatchBrowserEvent('alert', ['type' => 'success', 'message' => 'Auction approved.']);
     }
-    elseif($filterAuction=='pending')
-    {
-        $filter = 'list/pending/auction';
-        $filterType='pending';
-    }
-    else
-    {
-        $filter = 'list/all/auction';
-        $filterType='all';
-    }
-    $url = baseUrl().$filter;
-    $data = makeCurlRequest($url, 'GET');
-    $this->filterAuction = $data['data'];
-    $this->filterType = $filterType;
-    }
-    public function approved($id,$collection_id)
-    {
-        $urlCollectionStatus = baseUrl().'edit/collection/status/'.$collection_id;
-        makeCurlRequest($urlCollectionStatus, 'GET');
-        $url = baseUrl().'approve/auction/'.$id;
-        $data = makeCurlRequest($url, 'GET');
-            $this->dispatchBrowserEvent('alert', 
-                    ['type' => 'success',  'message' => ''.$data['message'].'']);
-    }
+
     public function rejected($id)
     {
-        $url = baseUrl().'reject/auction/'.$id;
-        $data = makeCurlRequest($url, 'GET');
-            $this->dispatchBrowserEvent('alert', 
-                    ['type' => 'success',  'message' => ''.$data['message'].'']);
+        // Replace this with your actual Eloquent code to update the status of the auction
+        $auction = Auction::find($id); // Replace "Auction" with your actual Eloquent model name
+        $auction->status = 'rejected';
+        $auction->save();
+
+        $this->dispatchBrowserEvent('alert', ['type' => 'success', 'message' => 'Auction rejected.']);
     }
- 
 }
