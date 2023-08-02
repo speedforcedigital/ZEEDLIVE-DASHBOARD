82a05133a4e8827ea741b83ca2df889c9cd832a6
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2023-08-03 01:03:30 +0300

    Fixing auctions page

--- resources/views/livewire/auctions.blade.php
+++ resources/views/livewire/auctions.blade.php
@@ -28,24 +28,24 @@
         <!-- Right: Actions -->
         <div class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
             <!-- Delete button -->
-            <!-- <x-actions.delete-button /> -->
+            <x-actions.delete-button />
             <!-- Add customer button -->
-            <!-- @if($verification_capability_exists)
+            @if($verification_capability_exists)
                 <button wire:click="filterAuction('all')" class="btn border-slate-200 hover:border-slate-300 {{ ($this->filterType == 'all' || $this->filterType == '') ? 'bg-indigo-500 text-white' : 'text-indigo-500' }}">All</button>
                 <button wire:click="filterAuction('pending')" class="btn border-slate-200 hover:border-slate-300 {{ ($this->filterType == 'pending') ? 'bg-indigo-500 text-white' : 'text-indigo-500' }}">Pending</button>
                 <button wire:click="filterAuction('verified')" class="btn border-slate-200 hover:border-slate-300 {{ ($this->filterType == 'verified') ? 'bg-indigo-500 text-white' : 'text-indigo-500' }}">Active</button>
                 <button wire:click="filterAuction('rejected')" class="btn border-slate-200 hover:border-slate-300 {{ ($this->filterType == 'rejected') ? 'bg-indigo-500 text-white' : 'text-indigo-500' }}">Rejected</button>
-            @endif -->
+            @endif
         </div>
     </div>
 
     <!-- Table -->
-    <!-- @if($verification_capability_exists)
+    @if($verification_capability_exists)
         <x-auctions.auctions-table :auctions="$auctions" :count="$total_auctions" />
-    @endif -->
+    @endif
 
     <!-- Pagination -->
     <div class="mt-8">
-        <!-- {{ $auctions->links() }} -->
+        {{ $auctions->links() }}
     </div>
 </div>
