20d3c618543c9c5aab035af77204374e890d7bb0
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2023-08-03 01:30:51 +0300

    Fixing auctions page

--- resources/views/components/auctions/auctions-table.blade.php
+++ resources/views/components/auctions/auctions-table.blade.php
@@ -4,6 +4,7 @@
     $delete_capability_exists = false;
 
     foreach ($permissionsArray as $item) {
+        dd($item);
         if (isset($item['Auction']) && is_array($item['Auction'])) {
             if (in_array('verification', $item['Auction'])) {
                 $verification_capability_exists = true;
