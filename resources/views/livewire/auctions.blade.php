e25f5f9f3db011e026399d2f73dac300e78ccd05
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2023-08-03 01:01:56 +0300

    Fixing auctions page

--- resources/views/livewire/auctions.blade.php
+++ resources/views/livewire/auctions.blade.php
@@ -1,4 +1,4 @@
-<!-- @php
+@php
     $permissionsArray = json_decode(Session::get('permissions'), true);
     $verification_capability_exists = false;
     $delete_capability_exists = false;
@@ -13,7 +13,7 @@
             }
         }
     }
-@endphp -->
+@endphp
 
 <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
     <!-- Page header -->
