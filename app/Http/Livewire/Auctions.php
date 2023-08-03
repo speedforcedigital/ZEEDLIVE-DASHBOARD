a319f16e193adf5907d9a0820717bc9687904789
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2023-08-03 03:12:36 +0300

    Fixing auctions page

--- app/Http/Livewire/Auctions.php
+++ app/Http/Livewire/Auctions.php
@@ -15,14 +15,14 @@ class Auctions extends Component
 
         // If filters are applied, adjust the query accordingly
         if ($this->filterType === 'verified') {
-            $auctions = DB::table('auctions')->where('status', 'verified')->paginate($perPage);
+            $auctions = DB::table('auction')->where('status', 'verified')->paginate($perPage);
         } elseif ($this->filterType === 'rejected') {
-            $auctions = DB::table('auctions')->where('status', 'rejected')->paginate($perPage);
+            $auctions = DB::table('auction')->where('status', 'rejected')->paginate($perPage);
         } elseif ($this->filterType === 'pending') {
-            $auctions = DB::table('auctions')->where('status', 'pending')->paginate($perPage);
+            $auctions = DB::table('auction')->where('status', 'pending')->paginate($perPage);
         } else {
             // Fetch all auctions without applying filters if no filter is selected
-            $auctions = DB::table('auctions')->paginate($perPage);
+            $auctions = DB::table('auction')->paginate($perPage);
         }
 
         $total_auctions = $auctions->total();
@@ -38,8 +38,8 @@ class Auctions extends Component
     public function approved($id, $collection_id)
     {
         // Update the status of the collection and auction
-        DB::table('collections')->where('id', $collection_id)->update(['status' => 'approved']);
-        DB::table('auctions')->where('id', $id)->update(['status' => 'approved']);
+        DB::table('my_collections')->where('id', $collection_id)->update(['status' => 'approved']);
+        DB::table('auction')->where('id', $id)->update(['status' => 'approved']);
 
         $this->dispatchBrowserEvent('alert', ['type' => 'success', 'message' => 'Auction approved.']);
     }
@@ -47,7 +47,7 @@ class Auctions extends Component
     public function rejected($id)
     {
         // Update the status of the auction
-        DB::table('auctions')->where('id', $id)->update(['status' => 'rejected']);
+        DB::table('auction')->where('id', $id)->update(['status' => 'rejected']);
 
         $this->dispatchBrowserEvent('alert', ['type' => 'success', 'message' => 'Auction rejected.']);
     }
