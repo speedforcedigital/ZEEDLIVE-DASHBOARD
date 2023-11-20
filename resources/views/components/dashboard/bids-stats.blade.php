a1793d02aaa83f2db1e108aa447a2400f564c1dd
saadi191 <saadtanveer115@gmail.com>
2023-11-20 16:36:23 +0500

    check

--- resources/views/components/dashboard/bids-stats.blade.php
+++ resources/views/components/dashboard/bids-stats.blade.php
@@ -1,7 +1,11 @@
-<div class="flex flex-col col-span-full sm:col-span-6 xl:col-span-2 bg-gradient-to-br from-amber-900 to-indigo-900 shadow-lg rounded-md border border-gray-300">
-<div class="px-5 pt-5">
+<div class="flex flex-col col-span-full sm:col-span-6 xl:col-span-2  shadow-lg rounded-md border border-gray-300"
+     style="
+    background: linear-gradient(to bottom right, #FFC107, #4F46E5);
+"
+>
+    <div class="px-5 pt-5">
         <header class="flex justify-center mb-2">
-            <a href="/auctions"><img src="{{ asset('images/icon-01.svg') }}" width="32" height="32" alt="Icon 01" /></a>
+            <a href="/auctions"><img src="{{ asset('images/icon-01.svg') }}" width="32" height="32" alt="Icon 01"/></a>
         </header>
         <h2 class="text-lg font-semibold text-white text-center mb-2">
             Live Bids
@@ -12,10 +16,11 @@
         </div>
     </div>
 </div>
-<div class="flex flex-col col-span-full sm:col-span-6 xl:col-span-2 bg-gradient-to-br from-indigo-700 to-gray-900 shadow-lg rounded-md border border-gray-300">
+<div
+    class="flex flex-col col-span-full sm:col-span-6 xl:col-span-2 bg-gradient-to-br from-indigo-700 to-gray-900 shadow-lg rounded-md border border-gray-300">
     <div class="px-5 pt-5">
         <header class="flex justify-center mb-2">
-            <a href="/auctions"><img src="{{ asset('images/icon-01.svg') }}" width="32" height="32" alt="Icon 01" /></a>
+            <a href="/auctions"><img src="{{ asset('images/icon-01.svg') }}" width="32" height="32" alt="Icon 01"/></a>
         </header>
         <h2 class="text-lg font-semibold text-white text-center mb-2">
             Regular Bids
@@ -27,13 +32,14 @@
     </div>
 </div>
 
-<div class="flex flex-col col-span-full sm:col-span-6 xl:col-span-2 bg-gradient-to-br from-gray-700 to-fuchsia-900 shadow-lg rounded-md border border-gray-300">
+<div
+    class="flex flex-col col-span-full sm:col-span-6 xl:col-span-2 bg-gradient-to-br from-gray-700 to-fuchsia-900 shadow-lg rounded-md border border-gray-300">
     <div class="px-5 pt-5">
         <header class="flex justify-center mb-2">
-            <a href="/auctions"><img src="{{ asset('images/icon-01.svg') }}" width="32" height="32" alt="Icon 01" /></a>
+            <a href="/auctions"><img src="{{ asset('images/icon-01.svg') }}" width="32" height="32" alt="Icon 01"/></a>
         </header>
         <h2 class="text-lg font-semibold text-white text-center mb-2">
-             Closed Bids
+            Closed Bids
         </h2>
         <div class="text-xs font-semibold text-slate-400 uppercase mb-1 text-center">Total Closed Bids</div>
         <div class="flex justify-center items-center mb-2">
