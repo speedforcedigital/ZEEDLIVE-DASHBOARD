101501837826a4ca8e441459bfd945609184af14
saadi191 <saadtanveer115@gmail.com>
2023-11-20 12:46:35 +0500

    dashboars

--- resources/views/components/dashboard/user-gender-graph.blade.php
+++ resources/views/components/dashboard/user-gender-graph.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white shadow-lg rounded-sm border border-slate-200">
     <header class="px-5 py-4 border-b border-slate-100">
-        <h2 class="font-semibold text-slate-800">Sessions By Gender</h2>
+        <h2 class="font-semibold text-slate-800">Users By Gender</h2>
     </header>
     <div class="grow flex flex-col justify-center">
         <div>
