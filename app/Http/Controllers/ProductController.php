b9232a5748573d68e68ea2e8f7629be40d930e5e
saadi191 <saadtanveer115@gmail.com>
2023-12-01 15:51:35 +0500

    orders table

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,8 +15,7 @@ class ProductController extends Controller
         $lot = Lot::where("id", $id)->with("auction")->first();
         $lot->image = Storage::disk('do')->url($lot->image);
 //        dd($lot);
-//        $lot->video = Storage::disk('do')->url($lot->video);
-
+        $lot->video = Storage::disk('do')->url($lot->video);
         $lot->auction->user->image = AccountDetail::where('user_id', $lot->auction->user->id)->first()->profile_image;
 //        dd($lot->auction->user->image);
         $lot->gallery_images = DB::table('product_galleries')->where('product_id', $id)->get();
