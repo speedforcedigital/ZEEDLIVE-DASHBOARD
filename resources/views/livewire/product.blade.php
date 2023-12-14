99a359b583812b53c1cde1c0a6bee3dd83d96643
saadi191 <saadtanveer115@gmail.com>
2023-12-14 15:47:48 +0500

    middleware

--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -1,9 +1,4 @@
-<?php
-   $permissions = Session::get('permissions');
-   if (is_null($permissions)) {
-       redirect()->route('login');
-   }
-?>
+
 
 <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
 
