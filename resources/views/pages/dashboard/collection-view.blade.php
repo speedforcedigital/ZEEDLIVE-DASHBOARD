4950674a3b8a1acfc0842d257fb616da97b3d892
saadi191 <saadtanveer115@gmail.com>
2024-01-01 17:21:24 +0500

    collection detail page

--- resources/views/pages/dashboard/collection-view.blade.php
+++ resources/views/pages/dashboard/collection-view.blade.php
@@ -1,9 +1,25 @@
 <x-app-layout>
     <div class="container mx-auto px-4 py-8">
+        <div class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
+            <a href="<?php echo url()->previous(); ?>"
+               class="btn-sm text-white hover:text-white bg-indigo-600 hover:bg-indigo-800">
+                <!-- back button icon -->
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-narrow-left"
+                     width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none"
+                     stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <line x1="5" y1="12" x2="15" y2="12"/>
+                    <line x1="5" y1="12" x2="9" y2="16"/>
+                    <line x1="5" y1="12" x2="9" y2="8"/>
+                </svg>
+                <span>Back</span>
+            </a>
+        </div>
         <div class="flex flex-wrap -mx-4">
 
             <!-- Product Media Gallery -->
-            <div class="w-full md:w-1/2 px-4 mb-4">
+            <div class="w-full md:w-1/2 px-4 mb-4 ml-8">
+
                 <!-- Image Slider (you can use a library like Swiper.js) -->
                 <div class="swiper-container product-slider">
                     <h4 class="text-xl font-semibold mb-2">Collection Image</h4>
@@ -12,144 +28,12 @@
                             <img src="{{  $collection->image }}" alt="Product Image"
                                  class="w-3/4 h-56  rounded-lg shadow-lg">
                         </div>
-                        <!-- Add more slides for additional images -->
-                    </div>
-                    <!-- Add pagination and navigation controls if needed -->
-{{--                                        <div class="swiper-pagination"></div>--}}
-{{--                                        <div class="swiper-button-next"></div>--}}
-{{--                                        <div class="swiper-button-prev"></div>--}}
-                </div>
 
-                <!-- Video Embed (you can use an iframe) -->
-                @if($collection->video)
-                    <div class="mt-4">
-                        <h4 class="text-xl font-semibold mb-2">Product Video</h4>
-                        <div class="relative">
-                            <video id="collectionVideo" controls width="100%" height="auto"
-                                   class="rounded-lg shadow-lg">
-                                <source src="{{ $collection->video }}" type="video/mp4">
-                                Your browser does not support the video tag.
-                            </video>
-                            <div id="playButtonContainer" class="absolute inset-0 flex items-center justify-center">
-                                <button id="playButton"
-                                        class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg">
-                                    Play Video
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-
-                @endif
-            </div>
-
-            <!-- Product Details -->
-            <div class="w-full md:w-1/2 px-4 mb-4">
-                <!-- back button on right side -->
-                <div class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
-                    <a href="<?php echo url()->previous(); ?>"
-                       class="btn-sm text-white hover:text-white bg-indigo-600 hover:bg-indigo-800">
-                        <!-- back button icon -->
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-narrow-left"
-                             width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none"
-                             stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                            <line x1="5" y1="12" x2="15" y2="12"/>
-                            <line x1="5" y1="12" x2="9" y2="16"/>
-                            <line x1="5" y1="12" x2="9" y2="8"/>
-                        </svg>
-                        <span>Back</span>
-                    </a>
-                </div>
-                <!-- Seller Information -->
-                <div class="mb-4">
-                    <div class="flex items-center">
-                        <img src="{{ $collection->user->accountDetail->profile_image }}" alt="Seller Avatar"
-                             class="w-12 h-12 rounded-full">
-                        <div class="ml-4">
-                            <h2 class="text-xl font-semibold"><a
-                                    href="{{ route('user.show',  $collection->user->id) }}"> {{ $collection->user->name }} </a>
-                            </h2>
-                            <!-- Seller details -->
-                        </div>
                     </div>
                 </div>
 
-                <!-- Title -->
-                <h1 class="text-3xl font-semibold mb-4">{{ $collection->title }}</h1>
-
-                <!-- Price -->
-
-                <!-- Bidding Details -->
-                <div class="mb-4">
-                    <h2 class="text-xl font-semibold mb-2">Offers</h2>
-                    <!-- Displaying offers from the collection -->
-                    @if(count($collection->offers) > 0)
-                        <h3 class="mt-4 text-lg font-semibold">Collection Offers</h3>
-                        <ul>
-                            @foreach($collection->offers as $offer)
-                                <li class="mt-2">
-                                    <p><strong>Offer Sender ID:</strong> {{ $offer->user->name }}</p>
-                                    <p><strong>Amount:</strong> {{ $offer->amount }} SAR</p>
-                                </li>
-                            @endforeach
-                        </ul>
-                    @else
-                        <p class="mt-4">No offers in this collection.</p>
-                    @endif
-                    @if(count($collection->offers) > 0)
-                        <h3 class="mt-4 text-lg font-semibold">Accepted Offers</h3>
-                        <ul>
-                            @foreach($collection->offers as $offer)
-                                @if($offer->is_accepted == "1")
-                                    <li class="mt-2">
-                                        <p><strong>Offer Sender ID:</strong> {{ $offer->user->name }}</p>
-                                        <p><strong>Amount:</strong> {{ $offer->amount }} SAR</p>
-                                    </li>
-                                @endif
-                            @endforeach
-                        </ul>
-                    @else
-                        <p class="mt-4">No Accepted offer in this collection.</p>
-                    @endif
-                </div>
-
-                <!-- Product Description -->
-                <div class="mb-4">
-                    <h2 class="text-xl font-semibold mb-2">Collection Description</h2>
-                    <p>{{ $collection->description }}</p>
-                </div>
-
-                <!-- Additional Details -->
-                <div class="mb-4">
-                    <h2 class="text-xl font-semibold mb-2">Additional Details</h2>
-                    <ul>
-                        <li>Gender: {{ $collection->gender }}</li>
-                        <li>Year: {{ $collection->year }}</li>
-                        <li>Color: {{ $collection->color }}</li>
-                        <li>Condition: {{ $collection->condition }}</li>
-                    </ul>
-                </div>
-
-                <!-- Add to Cart Button (or Bid Button) -->
-            </div>
-
-            <div class="w-full md:w-1/2 px-4 mb-4">
-{{--                                <div class="swiper-container gallery-slider mt-4">--}}
-{{--                                    <h4 class="text-xl font-semibold mb-2">Gallery Images</h4>--}}
-{{--                                    <div class="swiper-wrapper" style="height: 400px;">--}}
-{{--                                        @foreach($collection->gallery_images  as $image)--}}
-{{--                                            <div class="swiper-slide">--}}
-{{--                                                <img src="{{ $image->image }}" alt="Gallery Image" class="w-full h-full object-cover rounded-lg shadow-lg">--}}
-{{--                                            </div>--}}
-{{--                                        @endforeach--}}
-{{--                                    </div>--}}
-{{--                                    <!-- Add pagination and navigation controls if needed -->--}}
-{{--                                    <div class="slider-pagination"></div>--}}
-{{--                                    <div class="slider-button-next"></div>--}}
-{{--                                    <div class="slider-button-prev"></div>--}}
-{{--                                </div>--}}
                 <div class="max-w-2xl mx-auto">
-                    <h4 class="text-xl font-semibold mb-2">Gallery Images</h4>
+                    <h4 class="text-xl font-semibold mt-2">Gallery Images</h4>
 
                     <div id="default-carousel" class="relative" data-carousel="static">
                         <!-- Carousel wrapper -->
@@ -185,7 +69,7 @@
                 class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-blue-500 dark:bg-gray-800/30 group-hover:bg-blue-800 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                 <svg class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor"
                      viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round"
-                                                                                   stroke-linejoin="round"
+                                                                                  stroke-linejoin="round"
                                                                                   stroke-width="2"
                                                                                   d="M15 19l-7-7 7-7"></path></svg>
                 <span class="hidden">Previous</span>
@@ -208,7 +92,134 @@
 
 
                 </div>
+
             </div>
+
+
+
+            <!-- Product Details -->
+            <div class="bg-white dark:bg-slate-800 rounded-sm border border-slate-200 dark:border-slate-700 w-auto px-6 mb-4 ml-6">
+                <!-- back button on right side -->
+
+                <!-- Seller Information -->
+                <div class="mb-4 mt-2">
+                    <div class="flex items-center">
+                        <img src="{{ $collection->user->accountDetail->profile_image }}" alt="Seller Avatar"
+                             class="w-12 h-12 rounded-full">
+                        <div class="ml-4">
+                            <h2 class="text-xl font-semibold"><a
+                                    href="{{ route('user.show',  $collection->user->id) }}"> {{ $collection->user->name }} </a>
+                            </h2>
+                            <!-- Seller details -->
+                        </div>
+                    </div>
+                </div>
+
+
+                <!-- Title -->
+                <h1 class="text-3xl font-semibold mb-4">{{ $collection->title }}</h1>
+
+                <!-- Price -->
+                <div class="border rounded-lg p-2.5 mb-3">
+                    <!-- Bidding Details -->
+                    <div class="mb-4">
+                        <h2 class="text-xl font-semibold mb-2">Offers</h2>
+                        <!-- Displaying offers from the collection -->
+                        @if(count($collection->offers) > 0)
+                            <h3 class="mt-4 text-lg font-semibold">Collection Offers</h3>
+                            <ul>
+                                @foreach($collection->offers as $offer)
+                                    <li class="mt-2">
+                                        <p><strong>Offer Sender ID:</strong> {{ $offer->user->name }}</p>
+                                        <p><strong>Amount:</strong> {{ $offer->amount }} SAR</p>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        @else
+                            <p class="mt-4">No offers in this collection.</p>
+                        @endif
+                        @if(count($collection->offers) > 0)
+                            <h3 class="mt-4 text-lg font-semibold">Accepted Offers</h3>
+                            <ul>
+                                @foreach($collection->offers as $offer)
+                                    @if($offer->is_accepted == "1")
+                                        <li class="mt-2">
+                                            <p><strong>Offer Sender ID:</strong> {{ $offer->user->name }}</p>
+                                            <p><strong>Amount:</strong> {{ $offer->amount }} SAR</p>
+                                        </li>
+                                    @endif
+                                @endforeach
+                            </ul>
+                        @else
+                            <p class="mt-4">No Accepted offer in this collection.</p>
+                        @endif
+                    </div>
+                </div>
+                <div class="border rounded-lg p-2.5 mb-3">
+                    <!-- Product Description -->
+                    <div class="mb-4">
+                        <h2 class="text-xl font-semibold mb-2">Collection Description</h2>
+                        <p>{{ $collection->description }}</p>
+                    </div>
+                </div>
+
+                <!-- Additional Details -->
+                <div class="border rounded-lg p-2.5 mb-3">
+                    <div class="mb-4">
+                        <h2 class="text-xl font-semibold mb-2">Additional Details</h2>
+                        <ul>
+                            <li>Gender: {{ $collection->gender }}</li>
+                            <li>Year: {{ $collection->year }}</li>
+                            <li>Color: {{ $collection->color }}</li>
+                            <li>Condition: {{ $collection->condition }}</li>
+                        </ul>
+                    </div>
+                </div>
+
+
+                <!-- Add to Cart Button (or Bid Button) -->
+            </div>
+
+            <div class="w-full md:w-1/2 px-4 mb-4">
+                {{--                                <div class="swiper-container gallery-slider mt-4">--}}
+                {{--                                    <h4 class="text-xl font-semibold mb-2">Gallery Images</h4>--}}
+                {{--                                    <div class="swiper-wrapper" style="height: 400px;">--}}
+                {{--                                        @foreach($collection->gallery_images  as $image)--}}
+                {{--                                            <div class="swiper-slide">--}}
+                {{--                                                <img src="{{ $image->image }}" alt="Gallery Image" class="w-full h-full object-cover rounded-lg shadow-lg">--}}
+                {{--                                            </div>--}}
+                {{--                                        @endforeach--}}
+                {{--                                    </div>--}}
+                {{--                                    <!-- Add pagination and navigation controls if needed -->--}}
+                {{--                                    <div class="slider-pagination"></div>--}}
+                {{--                                    <div class="slider-button-next"></div>--}}
+                {{--                                    <div class="slider-button-prev"></div>--}}
+                {{--                                </div>--}}
+
+                <!-- Video Embed (you can use an iframe) -->
+                @if($collection->video)
+                    <div class="mt-4">
+                        <h4 class="text-xl font-semibold mb-2">Product Video</h4>
+                        <div class="relative">
+                            <video id="collectionVideo" controls width="100%" height="auto"
+                                   class="rounded-lg shadow-lg">
+                                <source src="{{ $collection->video }}" type="video/mp4">
+                                Your browser does not support the video tag.
+                            </video>
+                            <div id="playButtonContainer" class="absolute inset-0 flex items-center justify-center">
+                                <button id="playButton"
+                                        class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg">
+                                    Play Video
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                @endif
+            </div>
+
+
+
         </div>
     </div>
 </x-app-layout>
