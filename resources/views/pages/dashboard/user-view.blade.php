170e1fbcb8b3ca74a364b637cdbab995c2c3b4db
saadi191 <saadtanveer115@gmail.com>
2024-01-05 11:14:22 +0500

    user profile page

--- resources/views/pages/dashboard/user-view.blade.php
+++ resources/views/pages/dashboard/user-view.blade.php
@@ -76,14 +76,14 @@
                         <a class="block pb-3 text-slate-500 hover:text-slate-600 whitespace-nowrap tab-link"
                            href="#reviews">Reviews</a>
                     </li>
-                    <li class="mr-6 last:mr-0 first:pl-4 sm:first:pl-6 lg:first:pl-8 last:pr-4 sm:last:pr-6 lg:last:pr-8">
-                        <a class="block pb-3 text-slate-500 hover:text-slate-600 whitespace-nowrap tab-link"
-                           href="#orders">Orders</a>
-                    </li>
-                    <li class="mr-6 last:mr-0 first:pl-4 sm:first:pl-6 lg:first:pl-8 last:pr-4 sm:last:pr-6 lg:last:pr-8">
-                        <a class="block pb-3 text-slate-500 hover:text-slate-600 whitespace-nowrap tab-link"
-                           href="#offers">Offers</a>
-                    </li>
+{{--                    <li class="mr-6 last:mr-0 first:pl-4 sm:first:pl-6 lg:first:pl-8 last:pr-4 sm:last:pr-6 lg:last:pr-8">--}}
+{{--                        <a class="block pb-3 text-slate-500 hover:text-slate-600 whitespace-nowrap tab-link"--}}
+{{--                           href="#orders">Orders</a>--}}
+{{--                    </li>--}}
+{{--                    <li class="mr-6 last:mr-0 first:pl-4 sm:first:pl-6 lg:first:pl-8 last:pr-4 sm:last:pr-6 lg:last:pr-8">--}}
+{{--                        <a class="block pb-3 text-slate-500 hover:text-slate-600 whitespace-nowrap tab-link"--}}
+{{--                           href="#offers">Offers</a>--}}
+{{--                    </li>--}}
                 </ul>
             </div>
 
@@ -99,8 +99,7 @@
                         <div>
                             <h2 class="text-slate-800 font-semibold mb-2">About</h2>
                             <div class="text-sm space-y-2">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore magna aliqua</p>
+                                <p>{{$user->accountDetail->description}}</p>
                             </div>
                         </div>
 
@@ -132,8 +131,13 @@
             </section>
 
             <section id="collections" class="tab-content hidden">
-                <div>
+                <div id="collectionsContainer">
                     <h2 class="text-xl leading-snug text-slate-800 font-bold mb-2"> {{$user->name}}'s Collections</h2>
+                    @if($orders->count() == 0)
+                        <div class="flex flex-col items-center justify-center">
+                            <p class="text-gray-500 text-lg">No Collections Yet</p>
+                        </div>
+                    @endif
                     @foreach($collections as $collection)
                         <ul class="space-y-8 sm:space-y-5 my-6">
                             <!-- Related item -->
@@ -154,11 +158,10 @@
                                             <div class="flex space-x-1">
                                                 <!-- view collection button -->
                                                 <a class="btn-sm px-3 bg-white border-slate-200 hover:border-slate-300 text-slate-600"
-                                                   href="{{ route('collection.show',$collection->id ) }}">
+                                                   href="{{ route('collection.show',$collection->id) }}">
                                                     <span>View Collection</span>
                                                 </a>
                                             </div>
-
                                         </div>
                                         <div class="text-slate-400">·</div>
                                         <!-- Price -->
@@ -176,6 +179,7 @@
                     <div class="mt-2">
                         {{ $collections->links() }}
                     </div>
+
                 </div>
             </section>
 
@@ -200,58 +204,57 @@
 
 
                 <!-- Order History -->
-                <!-- Work History -->
                 <div>
                     <h2 class="text-slate-800 font-semibold mb-2">Order History</h2>
                     <div class="bg-white p-4 border border-slate-200 rounded-sm shadow-sm">
+                        @if($orders->count() == 0)
+                            <div class="flex flex-col items-center justify-center">
+                                <p class="text-gray-500 text-lg">No Orders Yet</p>
+                            </div>
+                        @endif
                         @foreach($orders as $order)
-                        <ul class="space-y-3">
+                            <ul class="space-y-3">
 
-                            <!-- Item -->
-                            <li class="sm:flex sm:items-center sm:justify-between">
-                                <div class="sm:grow flex items-center text-sm">
-                                    <!-- Icon -->
-                                    <div class="w-8 h-8 rounded-full shrink-0 bg-amber-500 my-2 mr-3">
-                                        <svg class="w-8 h-8 fill-current text-amber-50" viewBox="0 0 32 32">
-                                            <path d="M21 14a.75.75 0 0 1-.75-.75 1.5 1.5 0 0 0-1.5-1.5.75.75 0 1 1 0-1.5 1.5 1.5 0 0 0 1.5-1.5.75.75 0 1 1 1.5 0 1.5 1.5 0 0 0 1.5 1.5.75.75 0 1 1 0 1.5 1.5 1.5 0 0 0-1.5 1.5.75.75 0 0 1-.75.75Zm-7 10a1 1 0 0 1-1-1 4 4 0 0 0-4-4 1 1 0 0 1 0-2 4 4 0 0 0 4-4 1 1 0 0 1 2 0 4 4 0 0 0 4 4 1 1 0 0 1 0 2 4 4 0 0 0-4 4 1 1 0 0 1-1 1Z" />
-                                        </svg>
-                                    </div>
-                                    <!-- Position -->
-                                    <div>
-                                        <div class="font-medium text-slate-800">{{$order->lot->title}}</div>
-                                        <div class="flex flex-nowrap items-center space-x-2 whitespace-nowrap">
-                                            <div>Placed</div>
-                                            <div class="text-slate-400">·</div>
-                                            <div>{{$order->created_at->format('D j M, Y - g:i A')}}</div>
+                                <!-- Item -->
+                                <li class="sm:flex sm:items-center sm:justify-between">
+                                    <div class="sm:grow flex items-center text-sm">
+                                        <!-- Icon -->
+                                        <div class="w-8 h-8 rounded-full shrink-0 bg-amber-500 my-2 mr-3">
+                                            <svg class="w-8 h-8 fill-current text-amber-50" viewBox="0 0 32 32">
+                                                <path
+                                                    d="M21 14a.75.75 0 0 1-.75-.75 1.5 1.5 0 0 0-1.5-1.5.75.75 0 1 1 0-1.5 1.5 1.5 0 0 0 1.5-1.5.75.75 0 1 1 1.5 0 1.5 1.5 0 0 0 1.5 1.5.75.75 0 1 1 0 1.5 1.5 1.5 0 0 0-1.5 1.5.75.75 0 0 1-.75.75Zm-7 10a1 1 0 0 1-1-1 4 4 0 0 0-4-4 1 1 0 0 1 0-2 4 4 0 0 0 4-4 1 1 0 0 1 2 0 4 4 0 0 0 4 4 1 1 0 0 1 0 2 4 4 0 0 0-4 4 1 1 0 0 1-1 1Z"/>
+                                            </svg>
+                                        </div>
+                                        <!-- Position -->
+                                        <div>
+                                            <div class="font-medium text-slate-800">{{$order->lot->title}}</div>
+                                            <div class="flex flex-nowrap items-center space-x-2 whitespace-nowrap">
+                                                <div>Placed</div>
+                                                <div class="text-slate-400">·</div>
+                                                <div>{{$order->created_at->format('D j M, Y - g:i A')}}</div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <!-- Tags -->
-                                <div class="sm:ml-2 mt-2 sm:mt-0">
-                                    <ul class="flex flex-wrap sm:justify-end -m-1">
-                                        <li class="m-1">
-                                            <button class="inline-flex items-center justify-center text-xs font-medium leading-5 rounded-full px-2.5 py-0.5 border border-slate-200 hover:border-slate-300 shadow-sm bg-white text-slate-500 duration-150 ease-in-out">{{$order->total_amount}} SAR</button>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <hr>
+                                    <!-- Tags -->
+                                    <div class="sm:ml-2 mt-2 sm:mt-0">
+                                        <ul class="flex flex-wrap sm:justify-end -m-1">
+                                            <li class="m-1">
+                                                <button
+                                                    class="inline-flex items-center justify-center text-xs font-medium leading-5 rounded-full px-2.5 py-0.5 border border-slate-200 hover:border-slate-300 shadow-sm bg-white text-slate-500 duration-150 ease-in-out">{{$order->total_amount}}
+                                                    SAR
+                                                </button>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </li>
+                                <hr>
 
 
-                        </ul>
+                            </ul>
                         @endforeach
                     </div>
                 </div>
-{{--                <div class="bg-white p-4 border border-slate-200 rounded-sm shadow-sm mb-4">--}}
-{{--                    <h3 class="text-sm font-medium text-slate-800 mb-2">Order History</h3>--}}
-{{--                    <ul class="text-sm">--}}
-{{--                        @foreach($orders as $order)--}}
-{{--                            <li>Placed at {{ $order->created_at }} of Amount SAR {{ $order->total_amount }}</li>--}}
-{{--                        @endforeach--}}
-{{--                    </ul>--}}
-{{--                </div>--}}
 
-                {{--    <!-- Offers Sent -->--}}
                 <div class="bg-white p-4 border border-slate-200 rounded-sm shadow-sm mb-4 mt-2">
                     <h3 class="text-sm font-medium text-slate-800 mb-2">Offers Sent</h3>
                     <p class="text-sm">{{ $offers }} offers sent.</p>
@@ -261,58 +264,61 @@
             <section id="reviews" class="tab-content hidden">
                 <!-- Reviews -->
                 <div>
-                    <h2 class="text-xl leading-snug text-slate-800 font-bold mb-2">Reviews ({{$user->rating->count()}})</h2>
+                    <h2 class="text-xl leading-snug text-slate-800 font-bold mb-2">Reviews ({{$user->rating->count()}}
+                        )</h2>
                     @if($user->rating->count() == 0)
                         <div class="flex flex-col items-center justify-center">
                             <p class="text-gray-500 text-lg">No Reviews Yet</p>
                         </div>
                     @endif
                     @foreach($user->rating as $rating)
-                    <ul class="space-y-5 my-6">
-                        <!-- Review -->
-                        <li>
-                            <div class="flex items-center mb-2">
-                                <img class="w-8 h-8 rounded-full mr-3" src="{{$rating->user->accountDetail->profile_image ?? Null}}"
-                                     width="32" height="32" alt="User 07"/>
-                                <div>
-                                    <div class="text-sm font-semibold text-slate-800 mb-1">{{$rating->user->name}}</div>
-                                    <!-- Rating -->
-                                    <div class="flex items-center space-x-2">
-                                        <!-- Stars -->
-                                        <div class="flex space-x-1">
-                                            @for($i = 0; $i < $rating->rating; $i++)
-                                                <svg class="w-4 h-4 fill-current text-amber-500"
-                                                     viewBox="0 0 16 16">
-                                                    <path
-                                                        d="M8 0l2.245 5.527 5.955.515-4.527 3.927 1.36 5.88L8 11.763l-5.033 3.086 1.36-5.88L.8 6.042l5.955-.515L8 0z"/>
-                                                </svg>
-                                            @endfor
-                                        </div>
-                                        <!-- Rate -->
-                                        <div class="text-sm text-slate-600">
-                                            {{$rating->rating}}
+                        <ul class="space-y-5 my-6">
+                            <!-- Review -->
+                            <li>
+                                <div class="flex items-center mb-2">
+                                    <img class="w-8 h-8 rounded-full mr-3"
+                                         src="{{$rating->user->accountDetail->profile_image ?? Null}}"
+                                         width="32" height="32" alt="User 07"/>
+                                    <div>
+                                        <div
+                                            class="text-sm font-semibold text-slate-800 mb-1">{{$rating->user->name}}</div>
+                                        <!-- Rating -->
+                                        <div class="flex items-center space-x-2">
+                                            <!-- Stars -->
+                                            <div class="flex space-x-1">
+                                                @for($i = 0; $i < $rating->rating; $i++)
+                                                    <svg class="w-4 h-4 fill-current text-amber-500"
+                                                         viewBox="0 0 16 16">
+                                                        <path
+                                                            d="M8 0l2.245 5.527 5.955.515-4.527 3.927 1.36 5.88L8 11.763l-5.033 3.086 1.36-5.88L.8 6.042l5.955-.515L8 0z"/>
+                                                    </svg>
+                                                @endfor
+                                            </div>
+                                            <!-- Rate -->
+                                            <div class="text-sm text-slate-600">
+                                                {{$rating->rating}}
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="text-sm italic">“{{$rating->comment}}”
-                            </div>
-                        </li>
+                                <div class="text-sm italic">“{{$rating->comment}}”
+                                </div>
+                            </li>
 
-                    </ul>
+                        </ul>
 
                     @endforeach
 
                 </div>
             </section>
 
-            <section id="orders" class="tab-content hidden">
-                <p>orders tab content</p>
-            </section>
+{{--            <section id="orders" class="tab-content hidden">--}}
+{{--                <p>orders tab content</p>--}}
+{{--            </section>--}}
 
-            <section id="offers" class="tab-content hidden">
-                <p>offers tab content</p>
-            </section>
+{{--            <section id="offers" class="tab-content hidden">--}}
+{{--                <p>offers tab content</p>--}}
+{{--            </section>--}}
 
 
         </div>
