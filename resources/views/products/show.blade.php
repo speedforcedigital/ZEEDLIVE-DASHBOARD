c27583480112fb0eb71ee90176d949b7d4829be6
SPEEDFORCEDIGITAL <nouman@speedforce.agency>
2024-01-17 02:47:44 +0300

    Update

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -100,16 +100,16 @@
                         <ul>
                             <li class="flex items-center justify-between py-3 border-b border-slate-200">
                                 <div class="text-sm">Starting Bid</div>
-                                <div class="text-sm font-medium text-slate-800 ml-2">{{ $lot->auction->auction_start_price }} SAR</div>
+                                <div class="text-sm font-medium text-slate-800 ml-2">{{ number_format($lot->auction->auction_start_price, 2) }} SAR</div>
                             </li>
                             <li class="flex items-center justify-between py-3 border-b border-slate-200">
                                 <div class="text-sm">Current Bid</div>
                                 <div class="text-sm font-medium text-slate-800 ml-2">
-                                @if(count($lot->bids) > 0 )
-                                    {{$lot->bids->first()->amount }} SAR
-                                @else
-                                    No Bids
-                                @endif
+                                    @if(count($lot->bids) > 0)
+                                        {{ number_format($lot->bids->first()->amount, 2) }} SAR
+                                    @else
+                                        No Bids
+                                    @endif
                                 </div>
                             </li>
                             <li class="flex items-center justify-between py-3 border-b border-slate-200">
